706073a142e2114ed793767e181b38989fbbf230
aledu <aledu@AsusAle.mshome.net>
2023-02-13 20:47:25 -0600

    Semana 5: Se agrega el crud y plantillas

--- src/main/java/com/tienda_l/controller/ClienteController.java
+++ src/main/java/com/tienda_l/controller/ClienteController.java
@@ -2,12 +2,14 @@ package com.tienda_l.controller;
 
 import com.tienda_l.dao.ClienteDao;
 import com.tienda_l.domain.Cliente;
+import com.tienda_l.service.ClienteService;
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  *
@@ -17,16 +19,37 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ClienteController {
     
-    @Autowired
-    private ClienteDao clienteDao;
+    @Autowired//esta etiqueta se da cuenta de que ClienteService es una interface, entonces busca que clase usa
+    private ClienteService clienteService;
     
     @GetMapping("/")
     public String inicio(Model model){
-        var variable="Hola desde el back-End";
-        model.addAttribute("mensaje", variable);
-        
-        var clientes=clienteDao.findAll();
+        var clientes=clienteService.getClientes();
         model.addAttribute("clientes", clientes);
         return "index";
+    }
+    
+    @GetMapping("/cliente/nuevo")
+    public String clienteNuevo(Cliente cliente){
+        return "modificaCliente";
+    }
+    
+    @PostMapping("/cliente/guardar")
+    public String clienteGuardar(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/cliente/eliminar/{idCliente}")
+    public String clienteEliminar(Cliente cliente){
+        clienteService.delete(cliente);
+        return "redirect:/";
+    }
+    
+    @GetMapping("/cliente/modificar/{idCliente}")
+    public String clienteModificar(Model model, Cliente cliente){
+        cliente=clienteService.getCliente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "modificaCliente";
     }
 }
---
+++ src/main/java/com/tienda_l/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda_l.service.impl;
+
+import com.tienda_l.dao.ClienteDao;
+import com.tienda_l.domain.Cliente;
+import com.tienda_l.service.ClienteService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ *
+ * @author aledu
+ */
+@Service
+public class ClienteServiceImpl implements ClienteService{
+
+    @Autowired//Debe haber 1 solo objeto ClienteDao, si ya existe se usa y si no existe se crea automaticamente sin usar new.
+    private ClienteDao clienteDao;
+   
+    @Override
+    @Transactional(readOnly=true)
+    public List<Cliente> getClientes() {
+        return (List<Cliente>)clienteDao.findAll();
+    }
+    
+    @Transactional(readOnly=true)
+    @Override
+    public Cliente getCliente(Cliente cliente) {
+        return clienteDao.findById(cliente.getIdCliente()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Cliente cliente) {
+        clienteDao.save(cliente);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Cliente cliente) {
+        clienteDao.delete(cliente);
+    }
+}
